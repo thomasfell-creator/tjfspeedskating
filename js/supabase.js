463daeb32e92cc70dde218f6897a92a1c60c1f9e
Thomas Fell <thomas_fell@icloud.com>
2026-06-22 21:04:26 +0100

    js

---
+++ js/supabase.js
@@ -0,0 +1,11 @@
+const supabaseUrl = 'https://xpbnvqfmpfwjrqognlrc.supabase.co';
+
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwYm52cWZtcGZ3anJxb2dubHJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNDg1MTEsImV4cCI6MjA5NzcyNDUxMX0.EPQIOgIV09QYFzU39EyjLb19bSr79Ao8mq97cyZyUcg';
+
+const supabase = window.supabase.createClient(
+
+    supabaseUrl,
+
+    supabaseKey
+
+);
