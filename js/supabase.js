aa0dd052130acf1f8c664e1b54538b699b980195
Thomas Fell <thomas_fell@icloud.com>
2026-06-24 18:54:28 +0100

    git

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 const supabaseUrl ='https://xpbnvqfmpfwjrqognlrc.supabase.co';
 
-const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwYm52cWZtcGZ3anJxb2dubHJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNDg1MTEsImV4cCI6MjA5NzcyNDUxMX0.EPQIOgIV09QYFzU39EyjLb19bSr79Ao8mq97cyZyUcg';
+const supabaseKey ='sb_publishable_GhAbqKMMNfVT1oKlqpvbOg_-2CtJv-q';
 
 window.supabaseClient =
 
