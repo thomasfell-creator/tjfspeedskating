279b75304e165f3a7fada8aeef0a10c7439bbbc7
Thomas Fell <thomas_fell@icloud.com>
2026-06-24 17:08:28 +0100

    fix1

--- js/supabase.js
+++ js/supabase.js
@@ -1,27 +1,6 @@
-const supabaseUrl = 'https://xpbnvqfmpfwjrqognlrc.supabase.co';
+const supabaseUrl ='https://xpbnvqfmpfwjrqognlrc.supabase.co';
 
-const supabaseKey = 'YeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwYm52cWZtcGZ3anJxb2dubHJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNDg1MTEsImV4cCI6MjA5NzcyNDUxMX0.EPQIOgIV09QYFzU39EyjLb19bSr79Ao8mq97cyZyUcg';
-
-const { createClient } = supabase;
-
-const supabaseClient = createClient(
-
-    supabaseUrl,
-
-    supabaseKey
-
-);
-
-window.supabaseClient = supabaseClient;
-console.log("Supabase client created:", supabase);
-
-const supabaseUrl =
-
-'https://xpbnvqfmpfwjrqognlrc.supabase.co';
-
-const supabaseKey =
-
-'YeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwYm52cWZtcGZ3anJxb2dubHJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNDg1MTEsImV4cCI6MjA5NzcyNDUxMX0.EPQIOgIV09QYFzU39EyjLb19bSr79Ao8mq97cyZyUcg';
+const supabaseKey ='YeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwYm52cWZtcGZ3anJxb2dubHJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNDg1MTEsImV4cCI6MjA5NzcyNDUxMX0.EPQIOgIV09QYFzU39EyjLb19bSr79Ao8mq97cyZyUcg';
 
 window.supabaseClient =
 
