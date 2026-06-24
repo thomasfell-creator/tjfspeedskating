24fd0a47ce7e7484ad8f828f1f15d8f5d49b8016
Thomas Fell <thomas_fell@icloud.com>
2026-06-24 17:04:25 +0100

    fix

--- js/supabase.js
+++ js/supabase.js
@@ -13,3 +13,30 @@ const supabaseClient = createClient(
 );
 
 window.supabaseClient = supabaseClient;
+console.log("Supabase client created:", supabase);
+
+const supabaseUrl =
+
+'https://xpbnvqfmpfwjrqognlrc.supabase.co';
+
+const supabaseKey =
+
+'YeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwYm52cWZtcGZ3anJxb2dubHJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNDg1MTEsImV4cCI6MjA5NzcyNDUxMX0.EPQIOgIV09QYFzU39EyjLb19bSr79Ao8mq97cyZyUcg';
+
+window.supabaseClient =
+
+window.supabase.createClient(
+
+    supabaseUrl,
+
+    supabaseKey
+
+);
+
+console.log(
+
+    "Supabase client created:",
+
+    window.supabaseClient
+
+);
